38a52a0c12b1a3f1b7f78fa580cc47f61fde9822
singlaViact <shashwat.singla@viact.ai>
2021-09-02 21:06:40 +0800

    csv read

---
+++ src/components/EditableList/index.tsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+//Material-UI
+import { Grid, TextField } from "@material-ui/core";
+import Autocomplete from "@material-ui/lab/Autocomplete";
+
+function EditableList() {
+  const top100Films = [
+    { title: "The Shawshank Redemption", year: 1994 },
+    { title: "The Godfather", year: 1972 },
+    { title: "The Godfather: Part II", year: 1974 },
+    { title: "The Dark Knight", year: 2008 },
+  ];
+  return (
+    <Autocomplete
+      multiple
+      id="tags-standard"
+      options={top100Films}
+    //   getOptionLabel={(option) => option.title}
+      defaultValue={[top100Films[13]]}
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          variant="standard"
+          label="Multiple values"
+          placeholder="Favorites"
+        />
+      )}
+    />
+  );
+}
+export default EditableList;
--- src/views/DoctorListPage/index.tsx
+++ src/views/DoctorListPage/index.tsx
@@ -1,54 +1,49 @@
 import React, { useEffect, useState } from "react";
 //Material-UI
 import { Grid } from "@material-ui/core";
-import Papa from 'papaparse';
-import {readFileSync} from 'fs';
+import { csv } from "d3";
+// @ts-ignore
+import csvFilePath from "./doctors.csv";
+import EditableList from "../../components/EditableList";
 
 function DoctorListPage() {
-    const [cvsData,setCsvData] = useState('')
- 
-    // const loadData =()=>{
-    //     var csvFilePath = require("../../doctors.csv");
-    //     Papa.parse(csvFilePath, {
-    //       header: true,
-    //       download: true,
-    //       skipEmptyLines: true,
-    //       complete: function(results) {
-    //         console.log("res",results)
-    //       }
-    //     });
-    // }
-    // const fetchCsv=()=> {
-    //     return fetch('./doctors.csv').then(function (response:any) {
-    //         let reader = response.body.getReader();
-    //         let decoder = new TextDecoder('utf-8');
+  const [cvsData, setCsvData] = useState("");
 
-    //         return reader.read().then(function (result:any) {
-    //             return decoder.decode(result.value);
-    //         });
-    //     });
-    // }
-
-    const loadData = async()=>{
-        let csvData = require("./doctors.csv");
-        Papa.parse(csvData, {
-            complete: function(results) {
-                        console.log("res",results)
-                      }
-        });
-    }
-
-// useEffect(()=>{
-
-//     loadData()
-// },[])
+  useEffect(() => {
+    csv(csvFilePath).then((data) => {
+      console.log(data);
+      let region = [];
+      for (var i = 0; i < data.length; i++) {
+        if (region.indexOf(data[i].Region) < 0) {
+          region.push(data[i].Region);
+        }
+      }
+      // console.log("regio", region);
+      let locList: any = [];
+      // data.forEach(function (loc) {
+      //   if (loc.Region === val && locList.indexOf(loc.Location) < 0) {
+      //     let res = self.convert(loc);
+      //     locList.push(res.Location);
+      //   }
+      // });
+    });
+    // loadData()
+  }, []);
 
   return (
     <React.Fragment>
-      <Grid>"Sdfgdf"</Grid>
+      <Grid container spacing={4} justifyContent="space-between">
+        <Grid item md={4} sm={12} xs={12}>
+          <EditableList />
+        </Grid>
+        <Grid item md={4} sm={12} xs={12}>
+          <EditableList />
+        </Grid>
+        <Grid item md={4} sm={12} xs={12}>
+          <EditableList />
+        </Grid>
+      </Grid>
     </React.Fragment>
   );
 }
 export default DoctorListPage;
-
-
