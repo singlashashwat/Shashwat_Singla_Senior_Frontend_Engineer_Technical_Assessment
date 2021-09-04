836f669ce9303e9b3268bf8714d34169c3352fc0
singlaViact <shashwat.singla@viact.ai>
2021-09-04 20:06:12 +0800

    time

--- src/views/DoctorListPage/DoctorData.tsx
+++ src/views/DoctorListPage/DoctorData.tsx
@@ -5,7 +5,7 @@ import LocationOnIcon from "@material-ui/icons/LocationOn";
 //Interface
 import { Doctor } from "../../types/interfaces";
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(() => ({
   name: {
     marginTop: "0.35em",
   },
@@ -28,26 +28,34 @@ interface Props {
 function DoctorData(props: Props) {
   const classes = useStyles();
   const { data } = props;
-  console.log(data);
   let service_type = data?.["Service Type"].split("/");
   let price = data?.Price.split(",");
   let medicine = data?.Medicine.split(",");
-  //   let day_seq_1 =
-  //     data?.["Day Seq 1"].indexOf(",") > 0
-  //       ? data?.["Day Seq 1"].split(", ")
-  //       : data?.["Day Seq 1"];
-  //   let day_seq_1_time = data?.["Day Seq 1 time"];
-  //   let day_seq_2 = data?.["Day Seq 2"].split(",");
-  //   let day_seq_2_time = data?.["Day Seq 2 time"];
-  //   let day_seq_3 = data?.["Day Seq 3"].split(",");
-  //   let day_seq_3_time = data?.["Day Seq 3 time"];
-  //   let day_seq_4 = data?.["Day Seq 4"].split(",");
-  //   let day_seq_4_time = data?.["Day Seq 4 time"];
-  //   let weekdays = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
-  //   let getloop =
-  //     data?.["Day Seq 1"].indexOf("-") > 0
-  //       ? weekdays.indexOf(data?.["Day Seq 1"].split("-")[1]?.trim()) + 1
-  //       : 0;
+  let day_seq_1 = data?.["Day Seq 1"].split(new RegExp("[,/&]", "g"));
+  let day_seq_1_time = data?.["Day Seq 1 time"];
+  let day_seq_2 = data?.["Day Seq 2"].split(new RegExp("[,/&]", "g"));
+  let day_seq_2_time = data?.["Day Seq 2 time"];
+  let day_seq_3 = data?.["Day Seq 3"].split(new RegExp("[,/&]", "g"));
+  let day_seq_3_time = data?.["Day Seq 3 time"];
+  let day_seq_4 = data?.["Day Seq 4"].split(new RegExp("[,/&]", "g"));
+  let day_seq_4_time = data?.["Day Seq 4 time"];
+  let weekdays = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+  let getloop1 =
+    data?.["Day Seq 1"].indexOf("-") > 0
+      ? weekdays.indexOf(data?.["Day Seq 1"].split("-")[1]?.trim()) + 1
+      : 0;
+  let getloop2 =
+    data?.["Day Seq 2"].indexOf("-") > 0
+      ? weekdays.indexOf(data?.["Day Seq 2"].split("-")[1]?.trim()) + 1
+      : 0;
+  let getloop3 =
+    data?.["Day Seq 3"].indexOf("-") > 0
+      ? weekdays.indexOf(data?.["Day Seq 3"].split("-")[1]?.trim()) + 1
+      : 0;
+  let getloop4 =
+    data?.["Day Seq 4"].indexOf("-") > 0
+      ? weekdays.indexOf(data?.["Day Seq 4"].split("-")[1]?.trim()) + 1
+      : 0;
   return (
     <React.Fragment>
       <Divider />
@@ -81,20 +89,45 @@ function DoctorData(props: Props) {
 
       {service_type.length > 0 &&
         service_type.map((item, i) => (
-          <Typography variant="body2" className={classes.text}>
-            {item}: ${price[i]}{" "}
+          <Typography variant="body2" gutterBottom className={classes.text}>
+            {item}: ${price[i]}
             {!medicine[i] || medicine[i] === "NA"
               ? "(not inclusive medicine)"
-              : item === "HER"
-              ? "(inclusive " + medicine[i] + " Days of Chinese medicine)"
-              : item === "PHY"
-              ? "(inclusive " + medicine[i] + " Days of Western medicine)"
-              : medicine[i]}
+              : "(inclusive " + medicine[i] + " Days of " + item + " medicine)"}
           </Typography>
         ))}
-      {/* <Typography variant="body2" gutterBottom className={classes.text}>
-        {data?.["Day Seq 1"]}
-      </Typography> */}
+      <Typography variant="body2" gutterBottom className={classes.text}>
+        {getloop1 > 0
+          ? weekdays
+              .slice(0, getloop1)
+              .map(
+                (item, i) => (i > 0 ? ", " : "") + item + ": " + day_seq_1_time
+              )
+          : day_seq_1.map(
+              (item, i) => (i > 0 ? ", " : "") + item + ": " + day_seq_1_time
+            )}
+        {getloop2 > 0
+          ? weekdays
+              .slice(0, getloop2)
+              .map((item) => ", " + item + ": " + day_seq_2_time)
+          : day_seq_2_time
+          ? day_seq_2?.map((item) => ", " + item + ": " + day_seq_2_time)
+          : ""}
+        {getloop3 > 0
+          ? weekdays
+              .slice(0, getloop3)
+              .map((item) => ", " + item + ": " + day_seq_3_time)
+          : day_seq_3_time
+          ? day_seq_3?.map((item) => ", " + item + ": " + day_seq_3_time)
+          : ""}
+        {getloop4 > 0
+          ? weekdays
+              .slice(0, getloop4)
+              .map((item) => ", " + item + ": " + day_seq_4_time)
+          : day_seq_4_time
+          ? day_seq_4?.map((item) => ", " + item + ": " + day_seq_4_time)
+          : ""}
+      </Typography>
     </React.Fragment>
   );
 }
